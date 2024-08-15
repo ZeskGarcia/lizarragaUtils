cfaf464fccb0cd6a6efa7c9f8b7737b3c5b53a76
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2024-08-15 15:18:12 +0200

    Create WeaponsDamage.lua

---
+++ Client/WeaponsDamage.lua
@@ -0,0 +1,22 @@
+CreateThread(function()
+  while (Config.WeaponsDamage) do
+    Wait(0)
+    N_0x4757f00bc6323cfe(GetHashKey("WEAPON_UNARMED"), 0.4)
+    N_0x4757f00bc6323cfe(GetHashKey("WEAPON_NIGHTSTICK"), 0.1)
+    DisableControlAction(0, 44, true)
+  end
+end)
+
+CreateThread(function()
+  local _sleep = true
+  while (Config.WeaponsDamage) do
+    local _ped = PlayerPedId()
+    _sleep = not IsPedArmed(_ped)
+    if (not _sleep) then
+      DisableControlAction(1, 140, true)
+      DisableControlAction(1, 141, true)
+      DisableControlAction(1, 142, true)
+    end
+    Wait(_sleep and 750 or 0)
+  end
+end)
